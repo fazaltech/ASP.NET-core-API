e22d81baf7591e7178cdf1f054a92ef2620c8377
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2021-08-13 14:54:29 +0500

    Getting Authors of a Book with [HttpGet]

--- my-books/Data/ViewModels/BookVM.cs
+++ my-books/Data/ViewModels/BookVM.cs
@@ -28,5 +28,27 @@ namespace my_books.Data.ViewModels
         public List<int> AuthorIds { get; set; }
 
     }
+
+    public class BookWithAuthorsVM
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsRead { get; set; }
+
+        public DateTime? DateRead { get; set; }
+
+        public int? Rate { get; set; }
+
+        public string Genre { get; set; }
+
+        public string CoverUrl { get; set; }
+
+        public string PublisherName { get; set; }
+
+        public List<string> AuthorNames { get; set; }
+
+    }
 }
  
