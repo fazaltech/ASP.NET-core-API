d84a802882bd48e22648f04ffc287d6bd08a37d1
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2021-09-01 21:31:06 +0500

    Adding Server Side Paging

--- my-books/Controllers/PublishersController.cs
+++ my-books/Controllers/PublishersController.cs
@@ -26,14 +26,14 @@ namespace my_books.Controllers
         }
 
         [HttpGet("get-all-publishers")]
-        public IActionResult GetAllPublishers(string sortBy,string searchString) 
+        public IActionResult GetAllPublishers(string sortBy,string searchString, int pageNumber) 
         {
 
 
             try
             {
 
-                var _result = _publisherService.GetAllPublishers(sortBy, searchString);
+                var _result = _publisherService.GetAllPublishers(sortBy, searchString, pageNumber);
                 return Ok(_result);
 
             }
