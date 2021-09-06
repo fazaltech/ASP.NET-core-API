5fb7f450d953eaf78378da69d07306916fb363d0
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2021-09-06 06:50:59 +0500

    Start Unit Testing -Setting up Unit Testing Environment

---
+++ my-books-tests/PublishersServiceTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace my_books_tests
+{
+    public class PublishersServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
